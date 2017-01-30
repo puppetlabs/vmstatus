d0c81268b58e0255be8bb0c01c91e96800460d90
Josh Cooper <josh@puppetlabs.com>
2017-01-30 14:40:23 -0800

    Use tree query parameter to limit jenkins load
    Previously, vmstatus would retrieve all of the data for the Job and
    all of the data for the specified Run (either a specific build id or
    the last successful one).
    
    This commit uses the `tree` query parameter to limit the amount of data
    that needs to be generated and sent. It makes the queries faster[1], and
    hopefully reduces the amount of memory consumed by Jenkins, since it
    shouldn't need to load all of the Runs associated with the Job.
    
    Also add an HTTP user-agent string to identify what agents are hitting
    Jenkins.
    
    [1] https://www.cloudbees.com/blog/taming-jenkins-json-api-depth-and-tree

--- lib/vmstatus/jenkins_task.rb
+++ lib/vmstatus/jenkins_task.rb
@@ -2,13 +2,18 @@ require 'json'
 require 'rest-client'
 
 class Vmstatus::JenkinsTask
+  # limit jenkins queries for perf reasons
+  JOB_PARAMS = %w(buildable color inQueue)
+  BUILD_PARAMS = %w(result)
+  USER_AGENT = "vmstatus/#{Vmstatus::VERSION}"
+
   def initialize(url)
     @url = url
   end
 
   def run
     if @url =~ /https?:\/\/jenkins.*/
-      # hack since cinext tags don't include the build number
+      # hack since cinext tags only recently included the build number
       if @url =~ /\/\d+\/$/
         job_url = @url.sub(/\d+\/$/, '') + "api/json"
         build_url = @url + "api/json"
@@ -20,7 +25,7 @@ class Vmstatus::JenkinsTask
       #puts "job_url #{job_url}"
       #puts "build_url #{build_url}"
 
-      job_status = JSON.parse(RestClient.get(job_url))
+      job_status = JSON.parse(RestClient.get(job_url, params: {tree: JOB_PARAMS.join(',')}, :user_agent => USER_AGENT))
 
       if job_status['inQueue']
         'queued'
@@ -32,7 +37,7 @@ class Vmstatus::JenkinsTask
         'failed' # never run before
       else
         # not every job has a lastBuild
-        last_build_status = JSON.parse(RestClient.get(build_url))
+        last_build_status = JSON.parse(RestClient.get(build_url, params: {tree: BUILD_PARAMS.join(',')}, :user_agent => USER_AGENT))
         if last_build_status['result'] == 'SUCCESS'
           'passed'
         else
